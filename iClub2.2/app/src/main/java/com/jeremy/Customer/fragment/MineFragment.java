cb29bc56887930a647cdb1cfb6ccb703db3555f8
xiaoshengping <1277600115@qq.com>
2015-09-15 17:36:50 +0800

    xiaoshengping——9-  15  17：36

--- iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/MineFragment.java
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/MineFragment.java
@@ -6,15 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.jeremy.Customer.R;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MineFragment extends Fragment implements View.OnClickListener {
-    private Button button;
+public class MineFragment extends Fragment  {
+    @ViewInject(R.id.text)
+    private TextView textView;
+
 
     public MineFragment() {
         // Required empty public constructor
@@ -26,16 +30,13 @@ public class MineFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_mine, container, false);
-         button= (Button) getActivity().findViewById(R.id.button);
-        button.setOnClickListener(this);
+        //TextView textView= (TextView) getActivity().findViewById(R.id.text);
+        ViewUtils.inject(this,view);
+
+
         return view;
     }
 
-
-    @Override
-    public void onClick(View v) {
-
-    }
 
 
 }
