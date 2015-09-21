aeb46ea4953f517a2f2a1aa03aa10d1436dc6221
xiaoshengping <1277600115@qq.com>
2015-09-21 17:23:27 +0800

    小澎     09.21   16:52        //首页 修改

--- iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/HomeFragment.java
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/HomeFragment.java
@@ -30,7 +30,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_recruitment, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         ViewUtils.inject(this, view);
         initAdvertisement();
         return view;
