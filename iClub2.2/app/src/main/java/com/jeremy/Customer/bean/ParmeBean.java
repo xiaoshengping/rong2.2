4af95e67843954be6b592be0afc11aedf627bf53
xiaoshengping <1277600115@qq.com>
2015-09-21 15:39:32 +0800

    xiaoshengping——9-  21  15：39

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/bean/ParmeBean.java
@@ -0,0 +1,45 @@
+package com.jeremy.Customer.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by xiaoshengping on 2015/5/26.
+ */
+public class ParmeBean<T> implements Serializable {
+    private String state;
+    private String total;
+    private T value;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "ParmeBean{" +
+                "state='" + state + '\'' +
+                ", total='" + total + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
--- iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/MineFragment.java
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/MineFragment.java
@@ -1,23 +1,26 @@
 package com.jeremy.Customer.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.jeremy.Customer.R;
+import com.jeremy.Customer.uilt.LoginActivity;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MineFragment extends Fragment  {
-    @ViewInject(R.id.text)
-    private TextView textView;
+public class MineFragment extends Fragment implements View.OnClickListener {
+
+    @ViewInject(R.id.touxiang_iv)
+    private ImageView touXiangIv;
 
 
     public MineFragment() {
@@ -31,12 +34,29 @@ public class MineFragment extends Fragment  {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_mine, container, false);
         //TextView textView= (TextView) getActivity().findViewById(R.id.text);
-        ViewUtils.inject(this,view);
+        ViewUtils.inject(this, view);
 
-
+         intiView();
         return view;
     }
 
+    private void intiView() {
+        touXiangIv.setOnClickListener(this);
 
 
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.touxiang_iv:
+                Intent intent =new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+
+
+
+        }
+    }
 }
