368bb5b79c2f456f8efb818fa247ec7016ed116a
xiaoshengping <1277600115@qq.com>
2015-09-21 16:53:14 +0800

    小澎     09.21   16:52        //首页添加广告栏

--- iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/HomeFragment.java
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/HomeFragment.java
@@ -6,14 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.jeremy.Customer.R;
+import com.jeremy.Customer.view.SlideShowView;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    @ViewInject(R.id.advertisement_ll)
+    private LinearLayout advertisement_ll;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,7 +30,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_recruitment, container, false);
+        ViewUtils.inject(this, view);
+        initAdvertisement();
+        return view;
+    }
+
+    //初始化广告栏
+    private void initAdvertisement(){
+        SlideShowView ssv = new SlideShowView(getActivity());
+        advertisement_ll.addView(ssv);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int) (getResources().getDimension(R.dimen.advertisement_heitht)));
+        layoutParams.setMargins(0, 0, 0, 0);
+        ssv.setLayoutParams(layoutParams);
     }
 
 
