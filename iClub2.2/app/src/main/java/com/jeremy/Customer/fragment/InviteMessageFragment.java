f2ab709b95bbcf23e4e64153be2920669ada1ef1
xiaoshengping <1277600115@qq.com>
2015-09-24 15:40:53 +0800

    xiaoshengping---9--24---15:40

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/InviteMessageFragment.java
@@ -0,0 +1,31 @@
+package com.jeremy.Customer.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jeremy.Customer.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InviteMessageFragment extends Fragment {
+
+
+    public InviteMessageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_invite_message, container, false);
+    }
+
+
+}
