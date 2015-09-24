f7a29759740bfa107b51dc4ff2f019fb64581077
xiaoshengping <1277600115@qq.com>
2015-09-24 16:29:57 +0800

    小澎   9-24 14:29      //添加个人资料

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/uilt/TalentsDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.jeremy.Customer.uilt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jeremy.Customer.R;
+
+
+public class TalentsDetailsActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_talents_details);
+
+    }
+}
+
