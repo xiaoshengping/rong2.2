873c45e37e6e5447ab8e9a488b7201847f2f5c67
xiaoshengping <1277600115@qq.com>
2015-09-28 09:31:40 +0800

    xiaoshengping---9--28---9:08

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/fragment/TalentSucceedFragment.java
@@ -0,0 +1,31 @@
+package com.jeremy.Customer.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jeremy.Customer.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TalentSucceedFragment extends Fragment {
+
+
+    public TalentSucceedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_talent_succeed, container, false);
+    }
+
+
+}
