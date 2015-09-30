07dbfe1d0cbe86978a4ae9b3eb5388a6015be88f
xiaoshengping <1277600115@qq.com>
2015-09-30 10:36:48 +0800

    xiaoshengping---9--30---10:36

--- iClub2.2/app/src/main/java/com/jeremy/Customer/adapter/InviteMessageListAdapter.java
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/adapter/InviteMessageListAdapter.java
@@ -1,23 +1,41 @@
 package com.jeremy.Customer.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.jeremy.Customer.R;
 import com.jeremy.Customer.bean.mine.InviteMessgaeListValueBean;
+import com.jeremy.Customer.http.MyAppliction;
+import com.jeremy.Customer.url.AppUtilsUrl;
+import com.lidroid.xutils.HttpUtils;
 import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by xiaoshengping on 2015/7/2.
  */
 public class InviteMessageListAdapter extends  AppBaseAdapter<InviteMessgaeListValueBean> {
+
     private ViewHold viewHold;
+    private String status;
+    private int positions;
+    private String inviteid;
+    private static HashMap<Integer, Boolean> isSelected = new HashMap<Integer, Boolean>();
 
     public InviteMessageListAdapter(List<InviteMessgaeListValueBean> data, Context context) {
         super(data, context);
@@ -35,13 +53,16 @@ public class InviteMessageListAdapter extends  AppBaseAdapter<InviteMessgaeListV
             viewHold= (ViewHold) convertView.getTag();
 
         }
+
+        positions=position;
         inti(position);
         return convertView;
     }
 
-    private void inti(int position) {
+    private void inti(final int position) {
         viewHold.InviteResumeJobNameTv.setText(data.get(position).getInviteResume().getResumeJobName());
-       String status=data.get(position).getStatus();
+        status=data.get(position).getStatus();
+        inviteid=data.get(position).getInviteid();
         if (status.equals("0")){
             viewHold.InviteStatusTv.setText("new");
             viewHold.InviteStatusTv.setTextColor(context.getResources().getColor(R.color.textColor9a4274));
@@ -88,6 +109,121 @@ public class InviteMessageListAdapter extends  AppBaseAdapter<InviteMessgaeListV
         viewHold.inviteTripTimeTv.setText(data.get(position).getTripTime());
         viewHold.InviteJobCategoryTv.setText(data.get(position).getInviteResume().getResumeWorkPlace());
         viewHold.InviteCompanyNameTv.setText(data.get(position).getInvitePerson().getBEcompanyName());
+       /* viewHold.popupButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+
+
+                }else {
+
+
+                }
+            }
+        });*/
+
+
+        viewHold.acceptButtonTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (data.get(position).getStatus().equals("1")) {
+                    adoptData("3");
+
+                } else if (data.get(position).getStatus().equals("0")) {
+                    adoptData("1");
+                } else if (data.get(position).getStatus().equals("3") || data.get(position).getStatus().equals("4")) {
+                    // Intent intent=new Intent(context,);
+                    MyAppliction.showToast("成功接受");
+                }
+
+                MyAppliction.showToast(position+"kkkkkk"+data.get(position).getStatus());
+            }
+        });
+
+        viewHold.refuseButtonTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (data.get(position).getStatus().equals("1")) {
+                    adoptData("4");
+
+                } else if (data.get(position).getStatus().equals("0")) {
+                    adoptData("2");
+
+                }else if (data.get(position).getStatus().equals("3") || data.get(position).getStatus().equals("4")) {
+                    // Intent intent=new Intent(context,);
+                    MyAppliction.showToast("成功接受");
+                }
+                MyAppliction.showToast("jsjfhfhfss");
+            }
+        });
+
+    }
+    public static HashMap<Integer, Boolean> getIsSelected() {
+        return isSelected;
+    }
+
+    public static void setIsSelected(HashMap<Integer, Boolean> isSelected) {
+        InviteMessageListAdapter.isSelected = isSelected;
+    }
+    /*@Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.accept_button_tv:
+                if (data.get(positions).getStatus().equals("1")){
+                    adoptData("3");
+
+                }else if (status.equals("0")){
+                    adoptData("1");
+                }else if (status.equals("3")||status.equals("4")){
+                   // Intent intent=new Intent(context,);
+                    MyAppliction.showToast("成功接受");
+                }
+
+                MyAppliction.showToast(status);
+                break;
+            case R.id.refuse_button_tv:
+                if (status.equals("1")){
+                    adoptData("4");
+
+                }else if (status.equals("0")){
+                    adoptData("2");
+
+                }
+                MyAppliction.showToast("jsjfhfhfss");
+                break;
+
+        }
+
+
+
+
+    }*/
+
+
+    private void adoptData(String status) {
+        HttpUtils httpUtils =new HttpUtils();
+        RequestParams requestParams=new RequestParams();
+        requestParams.addBodyParameter("inviteid",inviteid );
+        requestParams.addBodyParameter("status", status);
+        httpUtils.send(HttpRequest.HttpMethod.POST, AppUtilsUrl.getAdoptAndRefuse(), requestParams, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                // Log.e("responseInfo",responseInfo.result);
+                if (responseInfo.result != null) {
+                    /*MyAppliction.showToast("成功接受");
+                    viewHold.acceptButtonTv.setVisibility(View.GONE);
+                    viewHold.refuseButtonTv.setVisibility(View.GONE);
+                    viewHold.deleteButtonTv.setVisibility(View.VISIBLE);*/
+                }
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                Log.e("onFailure", s);
+            }
+        });
+
+
     }
 
     private class ViewHold {
@@ -104,7 +240,13 @@ public class InviteMessageListAdapter extends  AppBaseAdapter<InviteMessgaeListV
         @ViewInject(R.id.accept_button_tv)
         private TextView acceptButtonTv;
         @ViewInject(R.id.refuse_button_tv)
-        private TextView refuseButtonTv;
+        private Button refuseButtonTv;
+        @ViewInject(R.id.delete_button_tv)
+        private Button deleteButtonTv;
+        @ViewInject(R.id.popup_button)
+        private CheckBox popupButton;
+        @ViewInject(R.id.accept_refuse_layout)
+        private RelativeLayout acceptRefuseLayout;
 
         public ViewHold(View view) {
             ViewUtils.inject(this, view);
