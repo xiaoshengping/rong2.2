57fa5b746e3928d9ab720b316f18911a429ee90b
xiaoshengping <1277600115@qq.com>
2015-10-10 16:29:12 +0800

    xiaoshengping---10--10---16:25

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/uilt/OneselfExperienceActivity.java
@@ -0,0 +1,17 @@
+package com.jeremy.Customer.uilt;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.jeremy.Customer.R;
+
+public class OneselfExperienceActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oneself_experience);
+    }
+
+
+}
