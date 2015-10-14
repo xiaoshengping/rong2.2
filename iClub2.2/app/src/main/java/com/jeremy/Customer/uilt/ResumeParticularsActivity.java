00277ef6c039dd57892931a7f59dc5f3a9d34946
xiaoshengping <1277600115@qq.com>
2015-10-14 14:47:32 +0800

    xiaoshengping---10--14---14:46

--- iClub2.2/app/src/main/java/com/jeremy/Customer/uilt/ResumeParticularsActivity.java
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/uilt/ResumeParticularsActivity.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import com.jeremy.Customer.R;
+import com.jeremy.Customer.view.CustomImageView;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 public class ResumeParticularsActivity extends ActionBarActivity {
+
+    @ViewInject(R.id.usericon_background_iv)
+    private CustomImageView customImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_particulars);
+        ViewUtils.inject(this);
+        customImageView.setBackgroundResource(R.mipmap.ic_launcher);
     }
 
 
