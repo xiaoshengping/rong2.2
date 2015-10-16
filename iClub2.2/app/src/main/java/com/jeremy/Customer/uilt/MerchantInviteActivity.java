a539ae720f8fe5e85d812abc059ce03b220ef85c
xiaoshengping <1277600115@qq.com>
2015-10-16 14:16:14 +0800

    xiaoshengping---10--16---14:46

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/uilt/MerchantInviteActivity.java
@@ -0,0 +1,17 @@
+package com.jeremy.Customer.uilt;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.jeremy.Customer.R;
+
+public class MerchantInviteActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchant_invite);
+    }
+
+
+}
