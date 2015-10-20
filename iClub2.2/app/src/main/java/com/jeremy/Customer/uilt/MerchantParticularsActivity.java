fc9f80a1aeba89a9019aadc7059035a80ac484ef
xiaoshengping <1277600115@qq.com>
2015-10-20 17:35:49 +0800

    xiaoshengping--10--20-17:32

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/uilt/MerchantParticularsActivity.java
@@ -0,0 +1,19 @@
+package com.jeremy.Customer.uilt;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jeremy.Customer.R;
+
+public class MerchantParticularsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchant_particulars);
+    }
+
+
+}
