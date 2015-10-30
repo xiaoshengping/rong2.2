75741c72a73246edf601a425442bb9d470bd31c2
xiaoshengping <1277600115@qq.com>
2015-10-30 15:03:13 +0800

    xiaoshengping--10--30-15:00

---
+++ iClub2.2/app/src/main/java/com/jeremy/Customer/uilt/CommentCountActivity.java
@@ -0,0 +1,16 @@
+package com.jeremy.Customer.uilt;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.jeremy.Customer.R;
+
+public class CommentCountActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_count);
+    }
+
+}
